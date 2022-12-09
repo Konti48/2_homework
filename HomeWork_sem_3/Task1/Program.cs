da8b4bb604c8e42f91dd2abf6bd87bf4ca40327f
Konti48 <92818968+Konti48@users.noreply.github.com>
2022-12-09 21:02:49 +0300

    Complete Task1 Math

--- HomeWork_sem_3/Task1/Program.cs
+++ HomeWork_sem_3/Task1/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите пятизначное число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int digit1, digit2, digit4, digit5;   // Использование математических функций
+digit1 = num / 10000;
+digit2 = num % 10000 / 1000;
+digit4 = num % 10000 % 1000 % 100 / 10;
+digit5 = num % 10000 % 1000 % 100 % 10;
+if (digit1 == digit5 && digit2 == digit4)
+{
+    Console.WriteLine("Введённое число является палиндромом");
+}
+else Console.WriteLine("Введённое число не является палиндромом");
