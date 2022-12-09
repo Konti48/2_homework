31d46a6dc1dd00f1fae6e44e03cea463b7cf6d84
Konti48 <92818968+Konti48@users.noreply.github.com>
2022-12-09 21:16:57 +0300

    Complete Task1 Array

--- HomeWork_sem_3/Task1/Program.cs
+++ HomeWork_sem_3/Task1/Program.cs
@@ -1,13 +1,19 @@
 ﻿Console.WriteLine("Введите пятизначное число: ");
 int num = Convert.ToInt32(Console.ReadLine());
-
-int digit1, digit2, digit4, digit5;   // Использование математических функций
-digit1 = num / 10000;
-digit2 = num % 10000 / 1000;
-digit4 = num % 10000 % 1000 % 100 / 10;
-digit5 = num % 10000 % 1000 % 100 % 10;
-if (digit1 == digit5 && digit2 == digit4)
+string arr = num.ToString();
+if (arr[0] == arr[arr.Length - 1] && arr[1] == arr[arr.Length -2])
 {
     Console.WriteLine("Введённое число является палиндромом");
 }
 else Console.WriteLine("Введённое число не является палиндромом");
+
+// int digit1, digit2, digit4, digit5;   // Использование математических функций
+// digit1 = num / 10000;
+// digit2 = num % 10000 / 1000;
+// digit4 = num % 10000 % 1000 % 100 / 10;
+// digit5 = num % 10000 % 1000 % 100 % 10;
+// if (digit1 == digit5 && digit2 == digit4)
+// {
+//     Console.WriteLine("Введённое число является палиндромом");
+// }
+// else Console.WriteLine("Введённое число не является палиндромом");
