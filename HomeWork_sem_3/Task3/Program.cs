582c907f9b9365c4d8432a469a8f54056f5a345b
Konti48 <92818968+Konti48@users.noreply.github.com>
2022-12-12 14:12:59 +0300

    Complete Task3 without method

--- HomeWork_sem_3/Task3/Program.cs
+++ HomeWork_sem_3/Task3/Program.cs
@@ -1,2 +1,12 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿Console.WriteLine("Введите число N: ");
+int num = Convert.ToInt32(Console.ReadLine());
+double[] array = new double[num];
+Console.Write("Массив кубов от 1 до N: [ ");
+for (int i = 0; i < num; i++)
+{
+array[i] = Math.Pow(i + 1, 3);
+Console.Write(array[i] + " ");
+}
+Console.Write("]");
+
+
