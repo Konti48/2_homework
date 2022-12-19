2c1ada6ac3e91e32d6781387624c1bf5b8f41f33
Konti48 <92818968+Konti48@users.noreply.github.com>
2022-12-19 20:30:56 +0300

    Complete Task1 hw5

--- HomeWork_sem_5/Task1/Program.cs
+++ HomeWork_sem_5/Task1/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int CountingEvenElementOfRandomArray(int length)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    int count = 0;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(100, 1000);
+        Console.WriteLine(array[i]);
+        if (array[i] % 2 == 0) count++;
+    }
+    Console.WriteLine(count);
+    return count;
+
+}
+CountingEvenElementOfRandomArray(7);
