1afc44758167ea339d6484dd8760c457045f2653
Konti48 <92818968+Konti48@users.noreply.github.com>
2022-12-19 21:45:10 +0300

    Complete Task3 hw5

--- HomeWork_sem_5/Task3/Program.cs
+++ HomeWork_sem_5/Task3/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿double FindDiffWithMaxMinElementsArray(double[] array)
+{
+    int iMax = 0;
+    int iMin = 0;
+    for (int i = 1; i < array.Length; i++)
+    {
+        if (array[i] > array[iMax]) iMax = i;
+        else if (array[i] < array[iMax]) iMin = i;
+    }
+    double diff = array[iMax] - array[iMin];
+    Console.WriteLine(diff);
+    return diff;
+}
+double[] arr = new double[] { 12, 4.13, 2.95, 15.98, 16, 17.47 };
+FindDiffWithMaxMinElementsArray(arr);
