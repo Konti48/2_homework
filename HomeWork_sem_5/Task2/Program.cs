08cdfe88a4f8efb43215f9a9b2fbb89d8bf1f389
Konti48 <92818968+Konti48@users.noreply.github.com>
2022-12-19 20:52:16 +0300

    Complete Task2 hw5

--- HomeWork_sem_5/Task2/Program.cs
+++ HomeWork_sem_5/Task2/Program.cs
@@ -1,2 +1,16 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int SumOddElementOfRandomArray(int length)
+{
+    int[] array = new int[length];
+    Random rnd = new Random();
+    int sum = 0;
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = rnd.Next(0, 100);
+        Console.WriteLine(array[i]);
+        if (i % 2 == 1) sum += array[i];
+    }
+    Console.WriteLine();
+    Console.WriteLine(sum);
+    return sum;
+}
+SumOddElementOfRandomArray(5);
