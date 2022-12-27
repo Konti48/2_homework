9a8d4570d2bf1f8510fa6e85ddf8e456d1e854ce
Konti48 <92818968+Konti48@users.noreply.github.com>
2022-12-27 20:05:24 +0300

    Complete Task1 hw6

--- HomeWork_sem_6/Task1/Program.cs
+++ HomeWork_sem_6/Task1/Program.cs
@@ -1,2 +1,11 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите количество числе для проверки:");
+int n = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+for (int i = 0; i < n; i++)
+{
+    Console.WriteLine($"Введите {i + 1}-е число");
+    int num = Convert.ToInt32(Console.ReadLine());
+    if (num > 0) count++;
+}
+Console.WriteLine("Количество чисел, которые больше 0, составляет: " + count);
